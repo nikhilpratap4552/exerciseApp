a20f5e8e75def0c19adf8773843248530fa71671
nikhilpratap4552 <140310015+nikhilpratap4552@users.noreply.github.com>
2024-10-04 23:20:08 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import AuthForm from '@/components/auth-form';
-import db from "./db";
+import db from '@/lib/db';
 
 export default async function Home({searchParams}) {
   const formMode = searchParams.mode || 'login';
